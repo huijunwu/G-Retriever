bd01de4f5852c7e6a918157c313a1d644a34725e
pig <huijunwu@users.noreply.github.com>
2025-07-13 03:19:22 -0700

    revert

--- src/utils/lm_modeling.py
+++ src/utils/lm_modeling.py
@@ -7,8 +7,7 @@ from transformers import AutoModel, AutoTokenizer
 from torch.utils.data import DataLoader
 import numpy as np
 
-# pretrained_repo = 'sentence-transformers/all-roberta-large-v1'
-pretrained_repo = 'sentence-transformers/all-MiniLM-L6-v2'
+pretrained_repo = 'sentence-transformers/all-roberta-large-v1'
 batch_size = 1024  # Adjust the batch size as needed
 
 
@@ -110,8 +109,7 @@ def load_sbert():
 
 def sber_text2embedding(model, tokenizer, device, text):
     if len(text) == 0:
-        # return torch.zeros((0, 1024))
-        return torch.zeros((0, 384))
+        return torch.zeros((0, 1024))
 
     encoding = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
     dataset = Dataset(input_ids=encoding.input_ids, attention_mask=encoding.attention_mask)
@@ -174,8 +172,7 @@ def contriever_text2embedding(model, tokenizer, device, text):
                 all_embeddings.append(embeddings)
             all_embeddings = torch.cat(all_embeddings, dim=0).cpu()
     except:
-        # all_embeddings = torch.zeros((0, 1024))
-        all_embeddings = torch.zeros((0, 384))
+        all_embeddings = torch.zeros((0, 1024))
 
     return all_embeddings
 
